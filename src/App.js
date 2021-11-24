1be34ab5c8367875724fb1069cee6d45de11f866
150866 <150866@ukr.net>
2021-11-24 17:11:07 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoList from "./Todo/TodoList";
 
 function App() {
+  const todos = [
+    {id: 1, completed: false, title: 'Купити хліб'},
+    {id: 2, completed: false, title: 'Купити масло'},
+    {id: 3, completed: false, title: 'Купити молоко'}
+
+  ]
+
+ function toggleTodo(id){
+   todos = todos.map(
+     todo => {
+       if (todo.id === id) {
+         todo.completed = !todo.completed
+       }
+       return todo
+     }
+   )
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <h1>React tutorial</h1>
+      <TodoList todos={todos} onToggle={toggleTodo} />
     </div>
+       
   );
 }
 
